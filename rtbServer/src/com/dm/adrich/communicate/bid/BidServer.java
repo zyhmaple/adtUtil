ceff26ce82caa0bd47a6a4ae132b29cf43985db1
zyhmaple <zyhmaple@sohu.com>
2018-10-23 16:33:33 +0800

    Change-Id: Id6490f2870d3a4102665c8e287b4e718c8981e69 Signed-off-by: zyhmaple <zyhmaple@sohu.com>

--- rtbServer/src/com/dm/adrich/communicate/bid/BidServer.java
+++ rtbServer/src/com/dm/adrich/communicate/bid/BidServer.java
@@ -1,67 +1,78 @@
-/*
- * Decompiled with CFR 0_123.
- * 
- * Could not load the following classes:
- *  Ice.Application
- *  Ice.Identity
- *  Ice.Object
- *  Ice.ObjectAdapter
- *  Ice.ObjectPrx
- *  org.apache.log4j.Logger
- */
 package com.dm.adrich.communicate.bid;
 
-import Ice.Application;
-import Ice.Identity;
-import Ice.Object;
-import Ice.ObjectAdapter;
-import Ice.ObjectPrx;
-import com.dm.adrich.communicate.bid.GeneralParseRequestServiceI;
-import com.dm.adrich.communicate.bid.util.DataTimerTask;
-import com.dm.adrich.communicate.bid.util.SystemInit;
-import java.io.PrintStream;
 import java.util.Timer;
-import java.util.TimerTask;
+
 import org.apache.log4j.Logger;
 
-public class BidServer
-extends Application {
-    protected static Logger log = null;
+import com.dm.adrich.communicate.bid.util.ConfigFileTimerTask;
+import com.dm.adrich.communicate.bid.util.DataTimerTask;
+import com.dm.adrich.communicate.bid.util.PDBTimeDealThread;
+import com.dm.adrich.communicate.bid.util.SysParams;
+import com.dm.adrich.communicate.bid.util.SystemInit;
 
-    public int run(String[] args) {
-        if (args.length > 3) {
-            System.err.println(String.valueOf(BidServer.appName()) + ": too many arguments");
-            return 1;
-        }
-        ObjectAdapter adapter = BidServer.communicator().createObjectAdapter("adrichBid");
-        log.info((java.lang.Object)"1111111111111");
-        adapter.add((Object)new GeneralParseRequestServiceI(), BidServer.communicator().stringToIdentity("bidAdapter"));
-        adapter.activate();
-        BidServer.communicator().waitForShutdown();
-        return 0;
+import Ice.Application;
+import Ice.ObjectAdapter;
+
+public class BidServer extends Application
+{
+  protected static Logger log = null;
+
+  public int run(String[] args)
+  {
+    if (args.length > 3)
+    {
+      System.err.println(appName() + ": too many arguments");
+      return 1;
     }
 
-    public static void main(String[] args) {
-        String confFilePath = "E:/adrichAllInstall/99/ice/conf/sys.properties";
-        if (args != null && args.length > 0) {
-            confFilePath = args[0];
-            System.out.println("\u65e5\u5fd7\u7684\u76ee\u5f55\u662f = " + args[1]);
-            System.setProperty("logDir", args[1]);
-            log = Logger.getLogger(BidServer.class);
-        }
-        try {
-            SystemInit.INSTANCE.contextInitialized(confFilePath);
-            Timer timer = new Timer();
-            timer.schedule((TimerTask)new DataTimerTask(), 1000, 600000);
-        }
-        catch (Exception e) {
-            log.error((java.lang.Object)e.getMessage(), (Throwable)e);
-            e.printStackTrace();
-        }
-        BidServer app = new BidServer();
-        int status = app.main("BidServer", args);
-        SystemInit.INSTANCE.contextDestroyed();
-        System.exit(status);
+    ObjectAdapter adapter = communicator().createObjectAdapter("adrichBid");
+    log.info("1111111111111");
+    adapter.add(new GeneralParseRequestServiceI(), communicator().stringToIdentity("bidAdapter"));
+    adapter.activate();
+    communicator().waitForShutdown();
+    return 0;
+  }
+
+  public static void main(String[] args)
+  {
+    String confFilePath = "E:/adrichAllInstall/99/ice/conf/sys.properties";
+    if ((args != null) && (args.length > 0))
+    {
+      confFilePath = args[0];
+      System.out.println("日志的目录是 = " + args[1]);
+      System.setProperty("logDir", args[1]);
+      log = Logger.getLogger(BidServer.class);
     }
+    try
+    {
+      SystemInit.INSTANCE.contextInitialized(confFilePath);
+
+      Timer timer = new Timer();
+
+      timer.schedule(new DataTimerTask(), 1000L, 600000L);
+
+      Thread.sleep(60000L);
+
+      if ("yes".equals(SysParams.sysProps.getProperty("isPDB")))
+      {
+        PDBTimeDealThread pdbtdt = new PDBTimeDealThread();
+
+        Thread pdbThread = new Thread(pdbtdt);
+
+        pdbThread.start();
+        Thread.sleep(10000L);
+      }
+
+      timer.schedule(new ConfigFileTimerTask(), 0L, 21600000L);
+    }
+    catch (Exception e) {
+      log.error(e.getMessage(), e);
+      e.printStackTrace();
+    }
+
+    BidServer app = new BidServer();
+    int status = app.main("BidServer", args);
+    SystemInit.INSTANCE.contextDestroyed();
+    System.exit(status);
+  }
 }
-
